15e7dbd200732b9abf5467a4e021970aa1e7e4ef
mingchun <alcohol49@gmail.com>
2016-12-21 00:32:55 +0800

    V 2.0
    # Conflicts:
    #	app/src/main/res/drawable/layer_list_badge.xml
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/alcohol/sharpdrawablebadge/MainActivity.java
+++ app/src/main/java/com/alcohol/sharpdrawablebadge/MainActivity.java
@@ -1,13 +1,40 @@
 package com.alcohol.sharpdrawablebadge;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean mRun;
+    private int mCount;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            ((TextView) findViewById(R.id.badge)).setText(String.valueOf(++mCount));
+
+            if (mRun) sendMessageDelayed(obtainMessage(), 100);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onIncrease(View view) {
+        if (mRun) {
+            mRun = false;
+            ((TextView) findViewById(R.id.increase)).setText("Increase");
+        } else {
+            mRun = true;
+            mHandler.obtainMessage().sendToTarget();
+            ((TextView) findViewById(R.id.increase)).setText("Stop");
+        }
     }
 }
